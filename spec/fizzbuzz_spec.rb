e4842188417fc655b6e1d8c40611b642726a7330
AnastasiiaBlaha <anastasiia.blaha@gmail.com>
2018-08-27 14:07:59 +0100

    Finished FizzBuzz first way

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,25 @@
+require "fizzbuzz"
+
+describe 'fizzbuzz' do
+ it 'returns "fizz" when passed 3' do
+   expect(fizzbuzz(3)).to eq 'fizz'
+ end
+ it 'returns "4" when passed 4' do
+   expect(fizzbuzz(4)).to eq 4
+ end
+ it 'returns "fizz" when passed 9' do
+   expect(fizzbuzz(9)).to eq 'fizz'
+ end
+ it 'returns "buzz" when passed 5' do
+   expect(fizzbuzz(5)).to eq 'buzz'
+ end
+ it 'returns "buzz" when passed 10' do
+   expect(fizzbuzz(10)).to eq 'buzz'
+ end
+ it 'return "fizzbuzz" when passed 15' do
+   expect(fizzbuzz(15)).to eq 'fizzbuzz'
+ end
+ it 'return "fizzbuzz" when passed 30' do
+   expect(fizzbuzz(30)).to eq 'fizzbuzz'
+ end
+end
